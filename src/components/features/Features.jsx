d5f1659577261de838e4e128ef44e0063aacd8d9
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2024-09-13 00:19:23 +0100

    Added the favicon for logo

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
+import React from "react";
 import { FaCode, FaDev, FaBook } from "react-icons/fa";
-import {SiAntdesign} from "react-icons/si";
-import Title from '../layouts/Title';
-import Card from './Card';
+import { SiAntdesign } from "react-icons/si";
+import Title from "../layouts/Title";
+import Card from "./Card";
 
 const Features = () => {
-  return (
-    <section
-      id="features"
-      className="w-full py-20 border-b-[1px] border-b-black"
-    >
-      <Title title="Features" des="What I Do" />
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
-        <Card
-          title="Backend Development"
-          des="I build robust backend systems with Python and Java, using Spring, Springboot
+	return (
+		<section
+			id="features"
+			className="w-full py-20 border-b-[1px] border-b-black"
+		>
+			<Title title="Features" des="What I Do" />
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
+				<Card
+					title="Backend Development"
+					des="I build robust backend systems with Python and Java, using Spring, Springboot
           and Django frameworks. I build systems that ensures data integrity and high performance."
-          icon={<FaDev />}
-        />
-        <Card
-          title="Frontend Development"
-          des="I create responsive and interactive user interfaces with React, HTML, CSS, and JavaScript,
+					icon={<FaDev />}
+				/>
+				<Card
+					title="Frontend Development"
+					des="I create responsive and interactive user interfaces with React, HTML, CSS, and JavaScript,
           I translate design mockups into functional, interactive web pages that are efficient and accessible."
-          icon={<FaCode />}
-        />
-        <Card
-          title="Technical Writing"
-          des="I document technical processes, create guides and tutorials, and write blog posts.
+					icon={<FaCode />}
+				/>
+				<Card
+					title="Technical Writing"
+					des="I document technical processes, create guides and tutorials, and write blog posts.
           My clear and comprehensive writing makes complex concepts accessible to both technical
           and non-technical stakeholders."
-          icon={<FaBook />}
-        />
-        <Card
-          title="System Design"
-          des="I design scalable, reliable, and maintainable system architectures, considering load balancing,
+					icon={<FaBook />}
+				/>
+				<Card
+					title="System Design"
+					des="I design scalable, reliable, and maintainable system architectures, considering load balancing,
           microservices, database design, and cloud infrastructure to optimize performance and resilience."
-          icon={<SiAntdesign />}
-        />
-      </div>
-    </section>
-  );
-}
+					icon={<SiAntdesign />}
+				/>
+			</div>
+		</section>
+	);
+};
 
-export default Features
+export default Features;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-scroll";
 // import { FiMenu } from "react-icons/fi";
 // import { MdClose } from "react-icons/md";
 // import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import {logo} from "../../assets/index"
-import { navLinksdata } from '../../constants';
+import { logo } from "../../assets/index";
+import { navLinksdata } from "../../constants";
 
 const Navbar = () => {
-  return (
-    <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div className={"h-[120px] w-[135px] flex justify-between items-center"}>
-          <div className={"border-[2px] border-solid rounded-full "}>
-              <img src={logo} alt="logo"/>
-          </div>
-          <p className={"text-lg text-lightText tracking-wide font-medium"}>IN BIO</p>
-      </div>
-      <div>
-        <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
-          {navLinksdata.map(({ _id, title, link }) => (
-            <li
-              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
-              key={_id}
-            >
-              <Link
-                activeClass="active"
-                to={link}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                {title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
+			<div className={"h-[120px] w-[135px] flex justify-between items-center"}>
+				<div className={"border-[2px] border-solid rounded-full "}>
+					<img src={logo} alt="logo" />
+				</div>
+				<p className={"text-lg text-lightText tracking-wide font-medium"}>
+					IN BIO
+				</p>
+			</div>
+			<div>
+				<ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
+					{navLinksdata.map(({ _id, title, link }) => (
+						<li
+							className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+							key={_id}
+						>
+							<Link
+								activeClass="active"
+								to={link}
+								spy={true}
+								smooth={true}
+								offset={-70}
+								duration={500}
+							>
+								{title}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
